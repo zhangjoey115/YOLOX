501f67d67fbcc0692939edb713f13d8bed796ca7
ZhangJiwei <zhangjiwei01@saicmotor.com>
2021-10-20 10:51:44 +0800

    zjw: commit missed dataset defination files

---
+++ yolox/data/datasets/tsr_zo_classes.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Copyright (c) Megvii, Inc. and its affiliates.
+
+# TT100K_CLASSES = ( '__background__",# always index 0
+TSR_ZO_CLASSES = (
+	"il",
+	"ph",
+	"pl",
+	"pr",
+	"other"
+)
